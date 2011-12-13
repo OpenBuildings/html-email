24025509116d3db65ee877731375bd938f46a528
Ivan Kerin <ivan@openbuildings.com>
2011-12-13 15:31:53 +0200

    add email messages sending

---
+++ classes/email.php
@@ -0,0 +1,216 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * Email module
+ *
+ * Ported from Kohana 2.2.3 Core to Kohana 3.0 module
+ * 
+ * Updated to use Swiftmailer 4.0.4
+ *
+ * @package    Core
+ * @author     Kohana Team
+ * @copyright  (c) 2007-2008 Kohana Team
+ * @license    http://kohanaphp.com/license.html
+ */
+class Email {
+
+	// SwiftMailer instance
+	protected static $_mailer;
+	protected static $_logger;
+	protected $_config;
+
+	/**
+	 * Creates a SwiftMailer instance.
+	 *
+	 * @param   string  DSN connection string
+	 * @return  object  Swift object
+	 */
+	public static function mailer($config = NULL)
+	{
+		if( ! self::$_mailer )
+		{
+			// Load default configuration
+			($config === NULL) and $config = Kohana::$config->load('html-email');
+
+			if ( ! class_exists('Swift_Mailer', FALSE))
+			{
+				require Kohana::find_file('vendor/swift', 'swift_required');
+			}
+			
+			switch ($config['driver'])
+			{
+				case 'smtp':
+
+					$transport = Swift_SmtpTransport::newInstance(
+						Arr::path($config, 'options.hostname', 'localhost'), 
+						Arr::path($config, 'options.port', 25),
+						Arr::path($config, 'options.encryption')
+					);
+					
+					$transport->setTimeout(Arr::path($config, 'options.timeout', 5));
+
+					if($user = Arr::path($config, 'options.username') AND $pass = $user = Arr::path($config, 'options.password'))
+					{
+						$transport->setUsername($user);
+						$transport->setPassword($pass);
+					}
+
+					break;
+
+				case 'sendmail':
+					$transport = Swift_SendmailTransport::newInstance(Arr::get($config, 'options'));
+					$transport->setTimeout(Arr::path($config, 'options.timeout', 5));
+					break;
+
+				default:
+					// Use the native connection
+					$transport = Swift_MailTransport::newInstance(Arr::get($config, 'options'));
+					break;
+			}
+
+			// Create the SwiftMailer instance
+			self::$_mailer = Swift_Mailer::newInstance($transport);
+
+			if(Arr::get($config, "logger"))
+			{
+				self::$_logger = new Swift_Plugins_Loggers_ArrayLogger();
+				self::$_mailer->registerPlugin(new Swift_Plugins_LoggerPlugin(self::$_logger));
+			}
+		}
+
+		return self::$_mailer;
+	}
+
+	static public function factory($subject, $config = null)
+	{
+		return new Email($subject, $config);
+	}
+
+	protected $_message;
+	protected $_attachments = array();
+
+	public function __construct($subject, $config = null)
+	{
+		self::mailer();
+
+		$this->_config = Arr::merge((array) Kohana::$config->load('html-email'), (array) $config);
+
+		$this->_message = Swift_Message::newInstance($subject);
+
+		if(Arr::get($config, 'from'))
+		{
+			$this->from($config['from']);
+		}
+
+		if(Arr::get($config, 'charset'))
+		{
+			$this->charset($config['from']);
+		}
+	}
+
+	public function html($body_view, $params = null)
+	{
+		$params = Arr::merge((array) $params, $this->_attachments);
+
+		if($this->_config['layout'])
+		{
+			$body = View::factory($this->_config['layout'], array(
+				'title' => $this->_message->getSubject(), 
+				'content' => View::factory($body_view, $params)
+			));
+		}
+		else
+		{
+			$body = View::factory($body_view, $params);
+		}
+		
+		if ( Arr::get($this->_config, 'inline_css'))
+		{
+			if ( ! class_exists('CSSToInlineStyles', FALSE))
+			{
+				// Load CSSToInlineStyles
+				require Kohana::find_file('vendor', 'csstoinlinestyles/css_to_inline_styles');
+			}
+
+			$body_html = new CSSToInlineStyles($body);
+			$body_html->setEncoding($this->_message->getCharset());
+			$body_html->setUseInlineStylesBlock();
+			$body = $body_html->convert();
+		}
+
+		$this->_message->addPart($body,  "text/html");
+		return $this;
+	}
+
+	public function plain($body_view, $params = null)
+	{
+		$params = Arr::merge((array) $params, $this->_attachments);
+
+		$body = View::factory($body_view, $params);
+
+		$this->_message->addPart($body, "text/plain");
+		return $this;
+	}
+
+	public function attach($name, $file)
+	{
+		$this->_attachemnts[$name] = $this->_message->attach(Swift_Attachment::fromPath($file));
+		return $this;
+	}
+
+	public function embed($name, $file)
+	{
+		$this->_attachemnts[$name] = $this->_message->embed(Swift_Attachment::fromPath($file));
+		return $this;
+	}
+
+	public function from($email, $name = null)
+	{
+		$this->_message->setFrom($email, $name);
+		return $this;
+	}
+
+	public function to($email, $name = null)
+	{
+		$this->_message->addTo($email, $name);
+		return $this;
+	}
+
+	public function cc($email, $name = null)
+	{
+		$this->_message->addCc($email, $name);
+		return $this;
+	}
+	
+	public function bcc($email, $name = null)
+	{
+		$this->_message->addBcc($email, $name);
+		return $this;
+	}
+
+	public function message()
+	{
+		return $this->_message;
+	}	
+
+	public function charset($charset)
+	{
+		$this->_message->setCharset($charset);
+		return $this;
+	}
+
+	public function send( $to = null )
+	{
+		if($to)
+		{
+			$this->to($to);
+		}
+
+		self::mailer()->send($this->_message);
+		
+		if(self::$_logger)
+		{
+			Log::instance()->add(Log::DEBUG, $this->_message->toString());
+			Log::instance()->add(Log::DEBUG, self::$_logger->dump());
+		}
+	}
+} // End email
---
+++ config/html-email.php
@@ -0,0 +1,52 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	/**
+	 * SwiftMailer driver, used with the email module.
+	 *
+	 * Valid drivers are: native, sendmail, smtp
+	 */
+	'driver' => 'native',
+	
+	/**
+	 * To use secure connections with SMTP, set "port" to 465 instead of 25.
+	 * To enable TLS, set "encryption" to "tls".
+	 * 
+	 * Note for SMTP, 'auth' key no longer exists as it did in 2.3.x helper
+	 * Simply specifying a username and password is enough for all normal auth methods
+	 * as they are autodeteccted in Swiftmailer 4
+	 * 
+	 * PopB4Smtp is not supported in this module as I had no way to test it but 
+	 * SwiftMailer 4 does have a PopBeforeSMTP plugin so it shouldn't be hard to implement
+	 * 
+	 * Encryption can be one of 'ssl' or 'tls' (both require non-default PHP extensions
+	 *
+	 * Driver options:
+	 * @param   null    native: no options
+	 * @param   string  sendmail: executable path, with -bs or equivalent attached
+	 * @param   array   smtp: hostname, (username), (password), (port), (encryption)
+	 */
+	'options' => NULL,
+	
+	/**
+	 * The layout view
+	 */
+	'layout'	=> NULL,
+
+	/**
+	 * Encoding charset
+	 */
+	'charset' => 'utf-8',
+
+	/**
+	 * Global from email address
+	 */
+	'from' => null,
+
+	'logger' => TRUE,
+
+	/**
+	 * If this is true css will be inlined
+	 */
+	'inline_css' => TRUE
+);
